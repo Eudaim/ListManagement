7273c42a2fe3c1f00114378fbbfd98566381685c
Eudaim <64391653+Eudaim@users.noreply.github.com>
2022-01-23 18:52:34 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManagement
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public DateTime DeadLine { get; set; }
+
+        public string Print()
+        {
+            return ( $"{Name} Completed: {IsCompleted}");
+        }
+
+
+    }
+}
